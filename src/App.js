67cbcc11d0912307c25042d9db30bc14fedad4a6
Vishnu <vappu5429@gmail.com>
2023-03-03 12:23:35 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+import { ChatFeed, LoginForm } from "./components";
 
-function App() {
+const App = () => {
+  if (!localStorage.getItem("username")) {
+    return <LoginForm />;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="
+  e0817ac1-e410-490c-8896-4a735ffeed67"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
-}
+};
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as ChatFeed } from "./ChatFeed";
+export { default as MessageForm } from "./MessageForm";
+export { default as MyMessage } from "./MyMessage";
+export { default as TheirMessage } from "./TheirMessage";
+export { default as LoginForm } from "./LoginForm";
